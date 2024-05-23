8ecff2a5c9d2ac8ffcdee523faabe6b982299b93
essien emmanuel <essienemma300@gmail.com>
2024-05-23 02:27:49 +0100

    added  redis database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,9 +1,74 @@
 const { createClient } = require('redis');
+const redis = require('redis')
 
-function connectToDb() {
-  const client = createClient();
-  client.on('error', () => "Error:: Redis connection error");
-  client.on('connect', () => "- Connected to Redis Successfully 🎉");
+// function connectToDb() {
+//   try {
+//     const client = createClient({
+//       port: 6379,
+//       host: '127.0.0.1'
+//     });
+//     client.on('connect', () => console.log("- Connected to Redis Successfully 🎉"));
+//     client.on('ready', () => console.log('client connected to redis and ready to use'))
+//     client.on('error', (error) => {
+//       console.log("Error:: Redis connection error");
+//       console.log(error)
+//     });
+//     console.log('here');
+
+//     client.on('end', () => console.log('client disconnected'));
+//     process.on('SIGINT', () => client.quit());
+//     return client;
+//   } catch (error) {
+//     console.log(error)
+//   }
+// // }
+
+// exports.DatabaseConnect = connectToDb;
+const client = createClient(
+  // {
+  // port: 6379,
+  // host: '127.0.0.1'
+// }
+);
+client.on('connect', () => console.log("- Connected to Redis Successfully 🎉"));
+client.on('ready', () => console.log('client connected to redis and ready to use'))
+client.on('error', (error) => {
+  console.log("Error:: Redis connection error");
+  console.log( 'error', error)
+});
+console.log('here');
+
+client.on('end', () => console.log('client disconnected'));
+process.on('SIGINT', () => client.quit());
+
+class Database {
+  constructor() {
+    this.connectToRedis();
+  }
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  async connectToRedis() {
+    const client = createClient();
+    client.on('connect', () => console.log("- Connected to Redis Successfully 🎉"));
+    
+    client.on('ready', () => console.log('- client connected to redis and ready to use ✈'));
+    
+    client.on('error', (error) => {
+      console.log("Error:: Redis connection error");
+      console.log(error)
+    });
+    
+    client.on('end', () => console.log('client disconnected'));
+    process.on('SIGINT', () => client.quit());
+
+    await client.connect();
+  }
 }
 
-exports.DatabaseConnect = connectToDb;
+exports.Database = Database;
